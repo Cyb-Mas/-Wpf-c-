b7e7b9f8b34adb83bc9878349c4999738a2d3878
Cyb-Mas <159514990+Cyb-Mas@users.noreply.github.com>
2024-02-29 11:38:18 +0300

    full screen and exit
    вход в full screen и выход из программы через Esc
    
    Signed-off-by: Cyb-Mas <159514990+Cyb-Mas@users.noreply.github.com>

--- Змейка на Wpf/MainWindow.xaml.cs
+++ Змейка на Wpf/MainWindow.xaml.cs
@@ -18,7 +18,9 @@ namespace Змейка_на_Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
-        public List<int> ListResult { get; private set; }
+        #region Private Fields
+        private List<int> ListResult { get; set; }
+        
         private readonly Dictionary<GridValue, ImageSource> gridValToImage = new()
         {
             { GridValue.Empty, Images.Empty },
@@ -33,23 +35,27 @@ namespace Змейка_на_Wpf
             { Direction.Down, 180 },
             { Direction.Left, 270 }
         };
-        int rows = new Random().Next(10, 40);
-        int cols = new Random().Next(10, 40);
+        private int rows = new Random().Next(10, 40);
+        private int cols = new Random().Next(10, 40);
         private readonly Image[,] gridImages;
         private GameState gameState;
         private bool gameRunning;
         private bool gamePaused = false;
+        private bool isFullScreen = false;
+        #endregion
 
+        #region Constructors
         public MainWindow()
         {
             InitializeComponent();
             gridImages = SetupGrid();
             gameState = new GameState(rows, cols);
             Pause.Visibility = Visibility.Hidden;
-            ListResult = new();
-            ListResult.Add(0);
+            ListResult = new() { 0 };
         }
+        #endregion
 
+        #region Methods
         private async Task RunGame()
         {
             Draw();
@@ -58,51 +64,7 @@ namespace Змейка_на_Wpf
             await GameLoop();
             await ShowGameOver();
             gameState = new GameState(rows, cols);
-            
-        }
-        private async void Window_PreviewKeyDown(object sender, KeyEventArgs e)
-        {
-            if (Overlay.Visibility == Visibility.Visible)
-            {
-                e.Handled = true;
-            }
 
-            if (!gameRunning)
-            {
-                gameRunning = true;
-                await RunGame();
-                gameRunning = false;
-            }
-        }
-
-        private void Window_KeyDown(object sender, KeyEventArgs e)
-        {
-            if (gameState.GameOver)
-            {
-                return;
-            }
-            switch (e.Key)
-            {
-                case Key.Left or Key.A:
-                    if (gameState.Dir != Direction.Right) 
-                        gameState.ChangeDirection(Direction.Left); 
-                    break;
-                case Key.Right or Key.D:
-                    if (gameState.Dir != Direction.Left)
-                        gameState.ChangeDirection(Direction.Right); 
-                    break;
-                case Key.Up or Key.W:
-                    if (gameState.Dir != Direction.Down)
-                        gameState.ChangeDirection(Direction.Up);
-                    break;
-                case Key.Down or Key.S:
-                    if (gameState.Dir != Direction.Up)
-                        gameState.ChangeDirection(Direction.Down);
-                    break;
-                case Key.Escape:
-                    if (gamePaused) gamePaused = false; else gamePaused = true;
-                    break;
-            }
         }
         private async Task GameLoop()
         {
@@ -118,32 +80,27 @@ namespace Змейка_на_Wpf
                 else
                 {
                     Pause.Visibility = Visibility.Visible;
-                    PauseText.Text = "PRESS ESCAPE TO CONTINUE";
+                    PauseText.Text = "PRESS ESCAPE TO CONTINUE OR PRESS ENTER TO EXIT";
                 }
             }
         }
-
         private Image[,] SetupGrid()
         {
             Image[,] images = new Image[rows, cols];
             GameGrid.Rows = rows;
-            GameGrid.Columns= cols;
+            GameGrid.Columns = cols;
             GameGrid.Width = GameGrid.Height * (cols / (double)rows);
-
             for (int r = 0; r < rows; r++)
             {
                 for (int c = 0; c < cols; c++)
                 {
-                    
-                        Image image = new Image
-                        {
-                            Source = Images.Empty,
-                            RenderTransformOrigin = new Point(0.5, 0.5)
-                        };
-                        images[r, c] = image;
-                        GameGrid.Children.Add(image);
-                    
-                    
+                    Image image = new()
+                    {
+                        Source = Images.Empty,
+                        RenderTransformOrigin = new Point(0.5, 0.5)
+                    };
+                    images[r, c] = image;
+                    GameGrid.Children.Add(image);
                 }
             }
             return images;
@@ -153,7 +110,7 @@ namespace Змейка_на_Wpf
             DrawGrid();
             DrawSnakeHead();
             ScoreText.Text = $"Score {gameState.Score}";
-            BestText.Text = $"Best {ListResult[ListResult.LastIndexOf(ListResult.Max())].ToString()}";
+            BestText.Text = $"Best {ListResult[ListResult.LastIndexOf(ListResult.Max())]}";
         }
         private void DrawGrid()
         {
@@ -161,43 +118,32 @@ namespace Змейка_на_Wpf
             {
                 for (int c = 0; c < cols; c++)
                 {
-               
                     GridValue gridVal = gameState.Grid[r, c];
                     gridImages[r, c].Source = gridValToImage[gridVal];
                     gridImages[r, c].RenderTransform = Transform.Identity;
-                   
                 }
             }
         }
         private void DrawSnakeHead()
         {
-            
-                Position headPos = gameState.HeadPosition();
-                Image image = gridImages[headPos.Row, headPos.Col];
-                image.Source = Images.Head;
-            
-                int rotation = dirToRotation[gameState.Dir];
-                image.RenderTransform = new RotateTransform(rotation);
-           
+            Position headPos = gameState.HeadPosition();
+            Image image = gridImages[headPos.Row, headPos.Col];
+            image.Source = Images.Head;
+            int rotation = dirToRotation[gameState.Dir];
+            image.RenderTransform = new RotateTransform(rotation);
         }
         private async Task DrawDeadSnake()
         {
             List<Position> positions = new List<Position>(gameState.SnakePositions());
-
             for (int i = 0; i < positions.Count; i++)
             {
                 Position pos = positions[i];
                 ImageSource source = (i == 0) ? Images.DeadHead : Images.DeadBody;
                 gridImages[pos.Row, pos.Col].Source = source;
                 if (i <= 10)
-                {
                     await Task.Delay(50);
-                }
-                if (i > 10)
-                {
+                else
                     await Task.Delay(20);
-                }
-                //await Task.Delay(40);
             }
         }
         private async Task ShowCountDown()
@@ -217,10 +163,76 @@ namespace Змейка_на_Wpf
             Overlay.Visibility = Visibility.Visible;
             OverlayText.Text = "PRESS ANY KEY TO START";
         }
-        private async Task Restart()
+        #endregion
+
+        #region EventsHandlers
+        private async void Window_KeyDown(object sender, KeyEventArgs e)
         {
-            
+            if (gameState.GameOver)
+            {
+                return;
+            }
+            switch (e.Key)
+            {
+                case Key.Left or Key.A:
+                    if (gameState.Dir != Direction.Right)
+                        gameState.ChangeDirection(Direction.Left);
+                    break;
+                case Key.Right or Key.D:
+                    if (gameState.Dir != Direction.Left)
+                        gameState.ChangeDirection(Direction.Right);
+                    break;
+                case Key.Up or Key.W:
+                    if (gameState.Dir != Direction.Down)
+                        gameState.ChangeDirection(Direction.Up);
+                    break;
+                case Key.Down or Key.S:
+                    if (gameState.Dir != Direction.Up)
+                        gameState.ChangeDirection(Direction.Down);
+                    break;
+                case Key.Escape:
+                    if (gamePaused) gamePaused = false; else gamePaused = true;
+                    break;
+                case Key.R:
+                    MainWindow mw = new();
+                    mw.Show();
+                    this.Close();
+                    Application.Current.MainWindow = mw;
+                    break;
+                case Key.Enter:
+                    if (gamePaused)
+                    {
+                        Application.Current.Shutdown();
+                    }
+                    break;
+                case Key.F11:
+                    if (!isFullScreen)
+                    {
+                        WindowState = WindowState.Maximized;
+                        isFullScreen = true;
+                    }
+                    else
+                    {
+                        WindowState = WindowState.Normal;
+                        isFullScreen = false;
+                    }
+                    break;
+
+                default:
+                    {
+                        if (Overlay.Visibility == Visibility.Visible)
+                            e.Handled = true;
+                        if (!gameRunning)
+                        {
+                            gameRunning = true;
+                            await RunGame();
+                            gameRunning = false;
+                        }
+                        break;
+                    }
+            }
         }
+        #endregion
         
     }
 }
